e49797d254dd317cf89c830518508215bf82131a
vitordex <vitordexjesus@gmail.com>
2020-07-31 12:01:43 -0300

    fix(api): Remove unnused files
    Issues: #2

--- src/auth/users/user.resources.ts
+++
@@ -1,5 +0,0 @@
-export interface IPutUserInput {
-    email?: string,
-    password?: string,
-    name?: string
-}
--- src/patients/patient.resources.ts
+++
@@ -1,5 +0,0 @@
-export interface IPutPatientInput {
-    email?: string,
-    password?: string,
-    name?: string
-}
