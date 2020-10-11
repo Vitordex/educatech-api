b5a4f6432b09ac126f20ff15be00dbb26c6fa754
Vitor Ribeiro <vitordexjesus@gmail.com>
2020-10-11 23:49:49 +0000

    feat(api): Mock email service
    Issues: #7

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import { AuthenticationMiddleware, TOKEN_LOCATION } from './authentication/authe
 import { JwtService } from './authentication/jwt.service';
 import { DBUserService } from "./auth/users/db-user.service";
 import { HashingService } from "./auth/users/hashing.service";
+import { EmailService } from './email-service/email.service';
 import { UserController } from './auth/users/user.controller';
 import { UserApi } from './auth/users/user.api';
 import { UserSchema } from './auth/users/user.schema';
@@ -81,13 +82,15 @@ export async function initApp(logger: Logger) {
     const userActionService = new DBUserActionService(dbContext);
     const actionService = new DBActionService(dbContext);
     const permissionService = new PermissionService(userActionService, actionService);
+    const emailService = new EmailService("test-user@test.com", "testPassword", "smtp.test.com", 465, "Test Educatech");
 
     const userController = new UserController(
         userService,
         jwtService,
         hashingService,
         roleService,
-        permissionService);
+        permissionService,
+        emailService);
     const userSchema = new UserSchema(InputValidation.BaseSchema);
     const userApi = new UserApi(
         userController,
--- src/email-service/email.service.ts
+++ src/email-service/email.service.ts
@@ -1,4 +1,4 @@
-import * as nodemailer from "nodemailer";
+// import * as nodemailer from "nodemailer";
 
 export class EmailService implements IEmailService {
     constructor(
@@ -9,22 +9,24 @@ export class EmailService implements IEmailService {
         private accountSenderName: string) {}
 
     public sendEmail(recipient: string, subject: string, emailBody: string) {
-        let transporter = nodemailer.createTransport({
-            host: this.accountHost,
-            port: this.accountPort,
-            secure: this.accountPort === 465,
-            auth: {
-                user: this.accountUser,
-                pass: this.accountPassword,
-            },
-        });
+        // let transporter = nodemailer.createTransport({
+        //     host: this.accountHost,
+        //     port: this.accountPort,
+        //     secure: this.accountPort === 465,
+        //     auth: {
+        //         user: this.accountUser,
+        //         pass: this.accountPassword,
+        //     },
+        // });
 
-        return transporter.sendMail({
-            from: `${this.accountSenderName} <${this.accountUser}>`,
-            to: recipient,
-            subject: subject,
-            html: emailBody
-        });
+        // return transporter.sendMail({
+        //     from: `${this.accountSenderName} <${this.accountUser}>`,
+        //     to: recipient,
+        //     subject: subject,
+        //     html: emailBody
+        // });
+
+        return Promise.resolve(this.accountHost + this.accountSenderName + this.accountPort + this.accountUser + this.accountPassword);
     }
 }
 
