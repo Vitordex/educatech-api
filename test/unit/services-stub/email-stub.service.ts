aed2b668e52b59fac5d4006b03665b62d7d214aa
Vitor Ribeiro <vitordexjesus@gmail.com>
2020-10-12 00:10:59 +0000

    fix(api): Add type to service stub
    Issues: #7

--- test/unit/services-stub/email-stub.service.ts
+++ test/unit/services-stub/email-stub.service.ts
@@ -2,7 +2,7 @@ import { IEmailService } from '../../../src/email-service/email.service';
 
 export class StubEmailService implements IEmailService {
     /* @ts-ignore */
-    sendEmail(recipient: string, subject: string, emailBody: string) {
+    sendEmail(recipient: string, subject: string, emailBody: string): Promise<any> {
         throw new Error("Method not implemented.");
     }
 }
